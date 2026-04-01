b1444f0166944bba1ddef73096ee730d1b8a3033
eesedla <eesedla@gmail.com>
2026-04-01 17:01:56 -0400

    initial commit

---
+++ chart.js
@@ -0,0 +1,52 @@
+const SCALE = 5;
+ 
+const PERSON_SVG = `<svg class="icon-person" viewBox="0 0 20 36" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="10" cy="5" r="4.5" fill="#c0392b"/>
+  <path d="M4 35 L4 16 Q4 10 10 10 Q16 10 16 16 L16 35 Z" fill="#c0392b"/>
+</svg>`;
+ 
+function personFaded(opacity) {
+  return `<svg class="icon-person" viewBox="0 0 20 36" xmlns="http://www.w3.org/2000/svg" style="opacity:${opacity}">
+  <circle cx="10" cy="5" r="4.5" fill="#c0392b"/>
+  <path d="M4 35 L4 16 Q4 10 10 10 Q16 10 16 16 L16 35 Z" fill="#c0392b"/>
+</svg>`;
+}
+ 
+function buildChart(data) {
+  const chart = document.getElementById('chart');
+ 
+  data.forEach(function(row) {
+    const fullIcons = Math.floor(row.officers / SCALE);
+    const remainder = row.officers % SCALE;
+    const partialOpacity = (remainder / SCALE).toFixed(2);
+ 
+    const rowEl = document.createElement('div');
+    rowEl.className = 'row';
+ 
+    const labelEl = document.createElement('div');
+    labelEl.className = 'row-label';
+    labelEl.innerHTML =
+      '<span class="charge-count">' + row.label + '</span>' +
+      '<span class="officer-count">' + row.officers.toLocaleString() + ' officers</span>';
+ 
+    const iconsEl = document.createElement('div');
+    iconsEl.className = 'icons';
+ 
+    var html = '';
+    for (var i = 0; i < fullIcons; i++) {
+      html += PERSON_SVG;
+    }
+    if (remainder > 0) {
+      html += personFaded(partialOpacity);
+    }
+ 
+    iconsEl.innerHTML = html;
+    rowEl.appendChild(labelEl);
+    rowEl.appendChild(iconsEl);
+    chart.appendChild(rowEl);
+  });
+}
+ 
+fetch('data.json')
+  .then(function(res) { return res.json(); })
+  .then(function(data) { buildChart(data); });
